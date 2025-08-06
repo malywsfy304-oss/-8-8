c63cf0764c4aafcaaa468c1e3dfe0dd2fed9f4c5
malywsfy304-oss <malywsfy304@gmail.com>
2025-08-07 02:30:27 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let dr = document.getElementById('bom');
+let ge = document.getElementById('tom');
+
+
+dr.onkeyup = function(){
+tom.value = bom.value * 425;
+}
+ge.onkeyup = function(){
+ bom.value = tom.value / 425;
+}
