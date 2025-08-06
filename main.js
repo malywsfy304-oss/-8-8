c87ecf742855f423a9823ee36720df1934fe5691
malywsfy304-oss <malywsfy304@gmail.com>
2025-08-07 03:16:27 +1300

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ let ge = document.getElementById('tom');
 
 
 dr.onkeyup = function(){
-tom.value = bom.value * ;
+tom.value = bom.value * 428;
 }
 ge.onkeyup = function(){
  bom.value = tom.value / 428;
 }
+
 
 
 
